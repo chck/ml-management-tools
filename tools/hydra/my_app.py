d39f349ab362c595542bde01c7ca343c8f9bb37b
chck <shimekiri.today@gmail.com>
2020-05-25 16:35:48 +0900

    Modify examples of hydra

--- tools/hydra/my_app.py
+++ tools/hydra/my_app.py
@@ -5,6 +5,11 @@ from omegaconf import DictConfig
 @hydra.main(config_path="config.yaml")
 def my_app(cfg: DictConfig) -> None:
     print(cfg.pretty())
+    params = cfg.model
+    print(params)
+    # model = get_model(params)
+    # model.fit(X, y)
+    # return r2_score(model.predict(X), y)
 
 
 if __name__ == "__main__":
