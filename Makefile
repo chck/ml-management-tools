807e969d2d62db618344e2d47d57f6bd311273c3
chck <shimekiri.today@gmail.com>
2020-06-01 05:25:50 +0900

    Add tutorial of mlflow

--- Makefile
+++ Makefile
@@ -15,6 +15,10 @@ vet:
 nb:
 	poetry run jupyter lab --allow-root --no-browser
 
+.PHONY: s   ## Run MLflow-server
+s:
+	poetry run mlflow server --default-artifact-root $(MLFLOW_STORAGE_PATH) --host 0.0.0.0
+
 .PHONY: help ## View help
 help:
 	@grep -E '^.PHONY: [a-zA-Z_-]+.*?## .*$$' $(MAKEFILE_LIST) | sed 's/^.PHONY: //g' | awk 'BEGIN {FS = "## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- tools/mlflow/mlflow_tracking.py
+++ tools/mlflow/mlflow_tracking.py
@@ -1,16 +1,45 @@
+import lightgbm as lgb
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import mlflow
 import os
-from mlflow import log_metric, log_param, log_artifact
 
-if __name__ == "__main__":
-    # Log a parameter (Key-value pair)
-    log_param("param1", 5)
+params = dict(
+    test_size=0.2,
+    random_state=3,
+)
 
-    # Log a metric; metrics can be updated throughout the run
-    log_metric("foo", 1)
-    log_metric("foo", 2)
-    log_metric("foo", 3)
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, **params)
+
+lgb_params = dict(
+    learning_rate=0.05,
+    n_estimators=500,
+)
+model = lgb.LGBMClassifier(**lgb_params)
+
+def mlflow_callback():
+    def callback(env):
+        for name, loss_name, loss_value, _ in env.evaluation_result_list:
+            mlflow.log_metric(key=loss_name, value=loss_value, step=env.iteration)
+    return callback
+
+mlflow.set_tracking_uri(os.environ["MLFLOW_HOST"])
+mlflow.set_experiment("MLMAN-1")
+with mlflow.start_run():
+    mlflow.log_params({**params, **lgb_params})
+    model.fit(
+        X_train,
+        y_train,
+        eval_set=(X_test, y_test),
+        eval_metric=["softmax"],
+        callbacks=[
+            lgb.early_stopping(10),
+            mlflow_callback(),
+        ])
 
     # Log an artifact (output file)
     with open("output.txt", "w") as f:
         f.write("Hello world!")
-    log_artifact("output.txt")
+    mlflow.log_artifact("output.txt")
